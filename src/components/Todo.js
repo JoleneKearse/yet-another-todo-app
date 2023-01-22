1e4e28574cfcedf7ebfe2d06c2bfc8d16a5665e8
Jolene Kearse <pharaohnutz@gmail.com>
2023-01-22 13:33:03 -0500

    feat: finish Todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -11,8 +11,24 @@ const Todo = () => {
     <section className="todo-container">
       <div className="todo">
         <h1 className="header">Gonna Do It Todo's</h1>
-        <div></div>
+        <div>
+          <div>
+            <input
+              type="text"
+              placeholder="What's up for today?"
+              onChange={(e) => setTodo(e.target.value)}
+            />
+          </div>
+          <div className="btn-container">
+            <button type="submit" className="btn" onClick={addTodo}>
+              Submit
+            </button>
+          </div>
+        </div>
+        <div className="todo-content">...</div>
       </div>
     </section>
   );
 };
+
+export default Todo;
